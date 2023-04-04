9e0c1c3375c8d5348a978c9d1d40cde9a3e1c67c
stephen-nju <zhubinnju@outlook.com>
2023-04-04 21:34:56 +0800

    update telescope keymap

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -23,8 +23,9 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>fc', require('telescope.builtin').grep_string, { desc = '' })
+vim.keymap.set('n', '<leader>fs', require('telescope.builtin').live_grep, { desc = 'find string' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+--配置Telescope git 快捷键
